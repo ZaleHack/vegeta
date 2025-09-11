97a5a6d38c1494db4ad50e17509dc43a5acbafbf
ZaleHack <saliou.thiam@ism.edu.sn>
2025-09-11 15:28:44 +0000

    chore: specify postcss source

--- postcss.config.js
+++ postcss.config.js
@@ -3,4 +3,10 @@ export default {
     tailwindcss: {},
     autoprefixer: {},
   },
+  // Specify the source filename to avoid PostCSS warnings about missing `from`
+  // when processing CSS. Using `undefined` tells PostCSS not to expect a file
+  // path and suppresses the warning.
+  options: {
+    from: undefined,
+  },
 };
