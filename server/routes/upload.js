0e9fe195270bb229a54116c04ed78e1a475acbd2
ZaleHack <saliou.thiam@ism.edu.sn>
2025-10-10 09:22:23 +0000

    Increase upload size limit to 200MB

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -30,7 +30,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 50 * 1024 * 1024 // 50MB
+    fileSize: 200 * 1024 * 1024 // 200MB
   },
   fileFilter: (req, file, cb) => {
     const ext = path.extname(file.originalname).toLowerCase();
