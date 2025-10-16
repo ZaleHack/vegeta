88a3adb95cdcb476d9427324f8c6030a12f5cae4
ZaleHack <saliou.thiam@ism.edu.sn>
2025-10-16 08:29:03 +0000

    Revert "Merge pull request #709 from ZaleHack/codex/centraliser-appels-api-et-gestion-erreurs"

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,13 @@ import './index.css';
 import 'leaflet/dist/leaflet.css';
 import { setupEncryptedFetch } from './utils/payloadEncryption.ts';
 import { NotificationProvider } from './components/NotificationProvider.tsx';
-import { QueryClient, QueryClientProvider } from './app/query';
 
 setupEncryptedFetch();
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById('root')!).render(
   <NotificationProvider>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </NotificationProvider>
 );
