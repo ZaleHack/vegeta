c830ff664db38df8538f357f7b2538c914bc826e
ZaleHack <saliou.thiam@ism.edu.sn>
2026-01-20 13:25:09 +0000

    Normalize IMEI display in map popups

--- src/App.tsx
+++ src/App.tsx
@@ -89,6 +89,7 @@ import SoraLogo from './components/SoraLogo';
 import ConfirmDialog, { ConfirmDialogOptions } from './components/ConfirmDialog';
 import { useNotifications } from './components/NotificationProvider';
 import { normalizePreview, NormalizedPreviewEntry, BaseSearchHit } from './utils/search';
+import { normalizeImeiWithCheckDigit } from './utils/imei';
 import BtsPage, { BtsProvider } from './features/bts/BtsPage';
 
 const VisibleIcon = (props: React.SVGProps<SVGSVGElement>) => (
@@ -209,42 +210,6 @@ interface PhoneIdentifierResult {
   };
   devices: PhoneIdentifierDevice[];
 }
-
-const computeImeiCheckDigit = (baseImei: string): number => {
-  const digits = baseImei.replace(/\D/g, '');
-  if (digits.length !== 14) {
-    return 0;
-  }
-
-  let sum = 0;
-  for (let index = 0; index < digits.length; index += 1) {
-    const digit = Number(digits[index]);
-    if (Number.isNaN(digit)) {
-      return 0;
-    }
-
-    const isEvenPosition = (index + 1) % 2 === 0;
-    if (isEvenPosition) {
-      const doubled = digit * 2;
-      sum += doubled > 9 ? doubled - 9 : doubled;
-    } else {
-      sum += digit;
-    }
-  }
-
-  return (10 - (sum % 10)) % 10;
-};
-
-const normalizeImeiWithCheckDigit = (imei: string): string => {
-  const digits = imei.replace(/\D/g, '');
-  if (digits.length < 14) {
-    return imei;
-  }
-
-  const base = digits.slice(0, 14);
-  const checkDigit = computeImeiCheckDigit(base);
-  return `${base}${checkDigit}`;
-};
 
 const extractHitsFromPayload = (hits: RawHitsPayload): RawSearchResult[] => {
   if (Array.isArray(hits)) {
