013b189315db4a5bf62df12a47692432c8d45270
ZaleHack <saliou.thiam@ism.edu.sn>
2026-03-12 14:21:55 +0000

    fix(vite): remove deprecated react-babel esbuild option

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        compact: true,
-      },
-    }),
+    react(),
   ],
   server: {
     host: true,
